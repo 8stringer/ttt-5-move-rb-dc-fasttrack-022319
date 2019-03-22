4faac3e134e0cd1e7ecc251376629acc62a76b09
Alex Orr <aorr75@gmail.com>
2019-03-22 22:05:15 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,6 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-def move(board, index, character)
-  board[index] = character
-  return board
+def move(array, index, character)
+  array[index] = character
 end
